a0f8e0fab2c2f00780ac11056d4d564acc6b78ac
Sandpitturtleee <karolmazur@onet.eu>
2026-03-28 12:04:09 +0100

    Adding scratch searching for radicals and meanings

--- kanji_radicals/main.py
+++ kanji_radicals/main.py
@@ -2,3 +2,21 @@ from functions import radicals_main
 
 if __name__ == "__main__":
     radicals_main()
+    # # Find all writing components (often called "radicals") of the character
+    # from jamdict import Jamdict
+    #
+    # jam = Jamdict()
+    # kanji = '雲'
+    # results = jam.krad[kanji]
+    # print(results)
+    #
+    # meaning = jam.lookup('雲')
+    # for c in meaning.chars:
+    #     print(c, c.meanings())
+    #
+    # print()
+    # for i in results:
+    #     result = jam.lookup(i)
+    #     for c in result.chars:
+    #         print(c, c.meanings())
+    #     print()
